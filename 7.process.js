c4a4a48b6ce44af7380184e51ea8edb8ddffda1c
RickyMaru <rrodriguezp@unsa.edu.pe>
2025-01-21 17:37:11 -0500

    Curso node dia 1

--- 7.process.js
+++ 7.process.js
@@ -5,9 +5,12 @@
 // process.exit(1)
 
 // podemos controlar eventos del proceso
-process.on('exit', () => {
+// process.on('exit', () => {
    // limpiar los recursos
-})
+// })
 
 // current working directory
 console.log(process.cwd())
+
+// platform
+console.log(process.env.PEPITO)
